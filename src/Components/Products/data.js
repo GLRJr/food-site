e947ca6407ec4a8f50cd56300471fb339e777e74
theresfuelinmycoffee <atigrouse.com>
2022-01-13 13:50:05 -0800

    initial commit

---
+++ src/Components/Products/data.js
@@ -0,0 +1,68 @@
+import product1 from "../../images/product1.jpg";
+import product2 from "../../images/product2.jpg";
+import product3 from "../../images/product3.jpg";
+import sweet1 from "../../images/sweet3.jpg";
+import sweet2 from "../../images/sweet-2.jpg";
+import sweet3 from "../../images/sweet-3.jpg";
+
+export const productData = [
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Supreme Pizza",
+    desc: "Marinara, basil, italian, roma, olives, and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Veggie Pizza",
+    desc: "Marinara, basil, italian, roma, olives, and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product2,
+    alt: "Pizza",
+    name: "Meat Lovers",
+    desc: "Marinara, basil, italian, roma, olives, and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product3,
+    alt: "Pizza",
+    name: "Hawiian",
+    desc: "Marinara, basil, italian, roma, olives, and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: "Donuts",
+    name: "Doughlicious",
+    desc: "Belgian chocolate glazed donuts covered in icing with sprinkles on top a fan-favorite",
+    price: "$9.99",
+    button: "Add to Cart",
+  },
+  {
+    img: sweet3,
+    alt: "Ice Cream",
+    name: "Caramel Wonder",
+    desc: "Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick",
+    price: "$12.99",
+    button: "Add to Cart",
+  },
+  {
+    img: sweet1,
+    alt: "Brownie",
+    name: "Brownie Bunch",
+    desc: "Double fudge brownie squares topped with white chocolate pieces and macadamia nuts",
+    price: "$9.99",
+    button: "Add to Cart",
+  },
+];
